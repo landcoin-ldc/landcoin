edf33c1891ae9926c39712a8cfd0af6044cc02d6
root <root@ip-172-16-24-181.cn-north-1.compute.internal>
2016-01-05 02:52:37 +0000

    Commit New Version Checks

--- src/miner.cpp
+++ src/miner.cpp
@@ -355,7 +355,7 @@ CBlock* CreateNewBlock(CReserveKey& reservekey, bool fProofOfStake, int64_t* pFe
             LogPrintf("CreateNewBlock(): total size %u\n", nBlockSize);
 
         if (!fProofOfStake)
-            pblock->vtx[0].vout[0].nValue = GetPOWReward(pindexPrev->nHeight +1, nFees) / 2;
+            pblock->vtx[0].vout[0].nValue = GetPOWReward(pindexPrev->nHeight +1, nFees);
 
 
         if (pFees)
